8d664c5a3a08eb9a676b7880bea0758839fbffd1
luelluelg <Luciana.Guzman@alu.frt.utn.edu.ar>
2026-05-01 23:59:36 -0300

    Problema 2  resuelto

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+
+        
+            public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+            {         
+                
+                decimal total;      //para el total
+            
+                if (quantity <= 0) {
+                    total = 0;         
+                }else { 
+                   total = quantity * unitPrice;         
+                }               
+                var resumen = new    {
+                    Code = productCode,             
+                    Description = productDescription,             
+                    Quantity = quantity,             
+                    Total = total         
+                };         
+                
+                string resultado = resumen.Code + "-" + resumen.Description + "-" + resumen.Total;         return resultado;    
+            } 
+    }
+
+            
+}
+
---
+++ Dsw2026Ej8/ProductHelperExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+   
+        public partial class ProductHelper
+        {
+     
+            private const string CulturaMoneda = "es-AR"; // dinero argentino
+
+   
+            private string FormatearPrecio(decimal precio)
+            {
+                CultureInfo cultura = new CultureInfo(CulturaMoneda);
+                return precio.ToString("C", cultura);
+            }
+        }
+    
+}
