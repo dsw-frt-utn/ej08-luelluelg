5405b240776c374a105b1c525e91796b0a1eb15a
luelluelg <Luciana.Guzman@alu.frt.utn.edu.ar>
2026-05-02 00:08:05 -0300

    Problema 6 resuelto

---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema6
+    {
+
+
+        public string NormalizarCodigoProducto(string code)
+        {
+    
+            if (code == null)
+            {
+                return "SIN-CODIGO";
+            }
+
+            
+            return code.ToProductCode();
+        }
+
+
+
+
+
+    }
+}
